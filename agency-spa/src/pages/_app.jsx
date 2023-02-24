0b9ac66222321b35026d2054ffd590027c7da9da
Damien <damienroot@icloud.com>
2023-02-24 15:39:22 +0000

    import commissioner font

--- agency-spa/src/pages/_app.jsx
+++ agency-spa/src/pages/_app.jsx
@@ -1,3 +1,11 @@
+import { Commissioner } from "next/font/google";
+
+const commissioner = Commissioner({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={commissioner.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
